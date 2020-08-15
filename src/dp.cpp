b21f31bfac1684410a3ce25ca7d877e482f3ff7e
zhaozz-lab <18046506891@163.com>
2020-08-15 20:15:21 +0800

    add tress

--- src/dp.cpp
+++ src/dp.cpp
@@ -97,6 +97,41 @@ void LongestCommonSubsequence()
 
 }
 
+
+//最长回文子串
+void longestPalindromicString()
+{
+    char S[10] = { 'a','b','c','b','c','b','a','f','g' };
+    int dp[10][10];
+    int len = strlen(S), ans = 1;
+    memset(dp, 0, sizeof(dp));
+    //边界
+    for (int i = 0; i < len; i++)
+    {
+        dp[i][i] = 1;
+        if (i < len - 1) {
+            if (S[i]==S[i+1])
+            {
+                dp[i][i + 1] = 1;
+                ans = 2;
+            }
+        }
+    }
+    //状态转移方程
+    for (int L = 3; L < len; L++) {//枚举子串的长度
+        for (int i = 0; i + L - 1 < len; i++) {
+            int j = i + L - 1;
+            if (S[i] == S[j] && dp[i+1][j-1]==1)
+            {
+                dp[i][j] = 1;
+                ans = L;
+            }
+        }
+    }
+    cout << "the ans is " << ans << endl;
+
+}
+
 int main()
 {
     cout << F(5) << endl;
@@ -112,5 +147,10 @@ int main()
 
     //最长公共子序列
     LongestCommonSubsequence();
+
+    //最长回文子串
+
+    longestPalindromicString();
+
     return 1;
 }
---
+++ src/trees.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+using namespace std;
+
+
+//给出一个m x n的矩阵，矩阵中的元素为0或1，称位置(x,y)与其上下左右四个位置(x,y-1),(x,y+1),(x+1,y),(x-1,y)。
+//如果矩阵中若干个1是相邻的，那么称这些1构成了一个块,求给定的矩阵中块的个数
+ /*0 1 1 1 0 0 1
+ 0 0 1 0 0 0 0 
+ 0 0 0 0 1 0 0
+ 0 0 0 1 1 1 0
+ 1 1 1 0 1 0 0
+ 1 1 1 1 0 0 0*/ 
+//如上图所示，块的个数为4
+using namespace std;
+
+const int maxn = 100;
+struct node
+{
+    int x, y;
+
+} Node;
+
+//int n, m;
+int m = 6;
+int n = 7;
+int matrix[6][7] = { {0, 1, 1, 1, 0, 0, 1},
+                         {0, 0 ,1, 0, 0, 0, 0},
+                         {0, 0, 0, 0, 1, 0, 0},
+                         {0, 0, 0, 1, 1, 1, 0},
+                         {1, 1, 1, 0, 1, 0, 0},
+                         {1, 1, 1, 1, 0, 0, 0}
+};
+//int matrix[maxn][maxn];
+bool inq[maxn][maxn] = { false };//记录位置(x,y)是否已如果队
+int X[4] = { 0,0,1,-1 };
+int Y[4] = { -1,1,0,0 };
+bool judge(int x, int y) {
+    //越界返回false
+    if (x >= n || x < 0 || y >= m || y < 0)
+        return false;
+    if (matrix[x][y] == 0 || inq[x][y] == true)
+        return false;
+    return true;
+}
+
+//BFS 函数访问位置(x,y)所在的块，将块中所有“1”的inq都设置为true
+
+void BFS(int x, int y)
+{
+    queue<node> Q;//定义队列
+    Node.x = x;
+    Node.y = y;
+    Q.push(Node);
+    inq[x][y] = true;
+    while (!Q.empty())
+    {
+        node top = Q.front();//取出队首元素
+        Q.pop(); //队首元素出队
+        //cout << top.x << top.y << endl;
+        for (int i = 0; i < 4; i++){//循环4次，得到4个相邻位置
+
+            int newX = top.x + X[i];
+            int newY = top.y + Y[i];
+            if (judge(newX, newY)) {
+                Node.x = newX;
+                Node.y = newY;
+                Q.push(Node);
+                inq[newX][newY] = true;
+            }
+
+        }
+    }
+ 
+}
+
+int main()
+{
+    int ans = 0;
+    for (int x = 0; x < n; x++)
+        for (int y = 0; y < m; y++)
+        {
+            if (matrix[x][y] == 1 && inq[x][y] == false) {
+                ans++;
+                BFS(x, y);
+            }
+        }
+    printf("%d\n", ans);
+    return 1;
+}
