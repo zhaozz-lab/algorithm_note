4fb68622367c94dc7fc34df5d743d31995c15f01
zhaozz-lab <18046506891@163.com>
2020-09-15 18:57:33 +0800

    Floyd

---
+++ src/graph_Bellman_Ford.cpp
@@ -0,0 +1,67 @@
+﻿// graph depth first traverse
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <set>
+const int MAXV = 510;
+const int INF = 0x3fffffff;
+using namespace std;
+struct Node {
+    int v, dis;//v为邻接边的目标顶点，dis为邻接边的边权
+    Node(int _v,int _dis):v(_v),dis(_dis){}
+};
+vector<Node> Adj[MAXV];
+
+int n, m, st, ed, weight[MAXV];
+
+int d[MAXV], w[MAXV], num[MAXV];
+set<int> pre[MAXV];//前驱
+
+void Bellman(int s) {
+    fill(d, d + MAXV, INF);
+    memset(num, 0, sizeof(num));
+    memset(w, 0, sizeof(w));
+
+    d[s] = 0;
+    w[s] = weight[s];
+    num[s] = 1;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int u = 0; u < n; u++) {
+            for (int j = 0; j < Adj[u].size(); j++) {
+                int v = Adj[u][j].v;
+                int dis = Adj[u][j].dis;
+                if (d[u] + dis < d[v]) {
+                    d[v] = d[u] + dis;
+                    w[v] = w[u] + weight[v];
+                    num[v] = num[u];
+                    pre[v].clear();
+                    pre[v].insert(u);
+               
+                }
+                else if(d[u] + dis == d[v])
+                {
+                    if (w[u] + weight[v] > w[v]) {
+                        w[v] = w[u] + weight[v];
+                    }
+                    pre[v].insert(u);
+                    num[v] = 0;
+                    set<int>::iterator it;
+                    for (it = pre[v].begin(); it != pre[v].end(); it++) {
+                        num[v] += num[*it];
+                    }
+
+                }
+
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    
+    
+    return 0;
+}
---
+++ src/graph_Floyd.cpp
@@ -0,0 +1,54 @@
+﻿// graph depth first traverse
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+const int INF = 1000000000;
+const int MAXV = 200;
+int n, m;
+int dis[MAXV][MAXV];
+
+void Floyd() {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dis[i][k] != INF && dis[k][j] != INF && dis[i][k] + dis[k][j] < dis[i][j])
+                {
+                    dis[i][j] = dis[i][k] + dis[k][j];
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    int u, v, w;
+
+    fill(dis[0], dis[0] + MAXV * MAXV, INF);
+    n = 6;
+    m = 8;
+    for (int i = 0; i < n; i++)
+        dis[i][i] = 0;
+     
+    dis[0][1] = 1;
+    dis[0][3] = 4;
+    dis[0][4] = 4;
+    dis[1][3] = 2;
+
+    
+    dis[2][5] = 1;
+    dis[3][2] = 2;
+    dis[3][4] = 3; 
+    dis[4][5] = 3;
+
+    Floyd();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", dis[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
