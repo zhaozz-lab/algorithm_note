14aec6013361b07c312abc9afef55ec4aaea83e9
zhaozzlab <18046506891@163.com>
2020-12-01 08:06:46 +0800

    virtural destructor

---
+++ src/cplusplus/src/union.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+
+class base {
+public:
+	base() {
+		cout << "construct the base class" << endl;
+	}
+
+	virtual ~base() {
+		cout << "destruct the base class" << endl;
+	}
+};
+
+class derived :public base {
+public:
+	derived() {
+		cout << "construct the derived class" << endl;
+	}
+
+	~derived() {
+		cout << "destruct the derived class" << endl;
+	}
+};
+
+void main() {
+
+	base* pbase = new base();
+	delete pbase;
+	derived* pderived = new derived();
+	delete pderived;
+	cout << "--------------" << endl;
+	base* pbasederived = new derived();
+	delete pbasederived;
+}
