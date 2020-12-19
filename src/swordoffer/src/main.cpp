4ab7a1772e7f68685bd60204906a25fe91e60e71
zhaozz-lab <18046506891@163.com>
2020-12-19 19:47:51 +0800

    the fourth

--- src/swordoffer/src/main.cpp
+++ src/swordoffer/src/main.cpp
@@ -104,10 +104,70 @@ void test_pointer_algorithm() {
 
 }
 
+bool less_than(int v1, int v2) {
+	return v1 < v2 ? true: false;
+}
+
+bool greater_than(int v1, int v2) {
+	return v1 > v2 ? true: false;
+}
+//泛型算法
+vector<int> filter(const vector<int>& vec, int filter_value, bool(*pred)(int, int)) {
+	vector<int> nvec;
+	for (int ix = 0; ix < vec.size(); ++ix)
+		if (pred(vec[ix], filter_value))
+			nvec.push_back(vec[ix]);
+	return nvec;
+}
+
+
+void testfilter() {
+	vector<int> input{ 1,2,3,4,5,6 };
+	vector<int> output = filter(input, 3, less_than);
+	//采用lambda表达式进行计算
+	vector<int> output1 = filter(input, 3, [](int x, int y) {return x < y; });
+}
+
+class Triangular {
+public:
+	int length() const { return _length; }
+	int beg_pos() const { return _beg_pos; }
+	int elem(int pos) const;
+
+	bool next(int& val);
+	void next_reset() {
+		_next = _beg_pos - 1;
+	}
+
+private:
+	int _length;
+	int _beg_pos;
+	int _next;
+	//static data member
+	vector<int> _elems;
+};
+
+
+int Triangular::elem(int pos) const 
+{
+	return _elems[pos - 1];
+}
+
+
+bool Triangular::next(int& value)
+{
+	if (_next < _beg_pos + _length -1)
+	{
+		value = _elems[_next];
+		return true;
+	}
+	return false;
+}
+
+
+
 int main() {
 	test_pointer_algorithm();
-
-
-
+	testfilter();
 
 }
